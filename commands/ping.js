d0ac4be642cecc434cb1ad69052dcd400684c139
Furkan Çentek <56794915+rootcf@users.noreply.github.com>
2022-01-15 02:40:04 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@
     name: "ping",
     description: "Get bot's ping",
     alias: [""],
+    owneronly : 0,
     run: async (client, message, args) =>{
         message.reply("`Pong! "+client.ws.ping+"ms`")
     }
